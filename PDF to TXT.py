a5af2a10945774187f2aa544ff1a51866b148954
Tomos Heighway <tomosheighway@gmail.com>
2022-06-12 14:05:12 +0100

    PDF to TXT converter

---
+++ PDF to TXT.py
@@ -0,0 +1,21 @@
+from PyPDF2 import PdfFileReader,PdfFileWriter
+
+file_path = "sample.pdf"
+pdf = PdfFileReader(file_path)
+file_name = file_path.strip('.pdf')+'.txt'
+
+with open(file_name, 'w') as file: 
+    for page_num in range(pdf.numPages):  
+        pageObj = pdf.getPage(page_num)
+        try:
+            txt = pageObj.extract_text()
+        except:
+            pass
+        else:
+            file.write('\n--------------- Page {0} ----------------'.format(page_num+1))
+            file.write('\n')
+            #file.write('' . center(5, '-'))
+            file.write(txt)
+    print("PDF with",pdf.numPages,"pages converted to txt and saved as", file_name)      
+    print(pdf.getDocumentInfo)  
+    file.close()
