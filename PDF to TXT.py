a91211fd0cf2ec97066994f99afa142589479ec3
Tomos Heighway <tomosheighway@gmail.com>
2022-06-12 14:06:44 +0100

    remove old cold

--- PDF to TXT.py
+++ PDF to TXT.py
@@ -14,7 +14,6 @@ with open(file_name, 'w') as file:
         else:
             file.write('\n--------------- Page {0} ----------------'.format(page_num+1))
             file.write('\n')
-            #file.write('' . center(5, '-'))
             file.write(txt)
     print("PDF with",pdf.numPages,"pages converted to txt and saved as", file_name)      
     print(pdf.getDocumentInfo)  
